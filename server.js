0fea6066c77b7c6cf34fdef81bbc911ecfffab78
Green Camper <stefan.palacios@gmail.com>
2023-05-28 01:32:06 -0500

    Finished server.js

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+//Port listening function
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+  });
+});
