9c4fcd9c5e8635d1cf9de1ff27fabd3b942b3868
Green Camper <stefan.palacios@gmail.com>
2023-05-30 00:07:05 -0500

    Added GET ALL thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,8 +4,29 @@ const { Thought } = require('../models');
 //export thought functions
 module.exports = { 
   //GET all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
   //GET one thought by its ID
+  async getThought(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.thoughtId })
+        .select("-__v")
 
+      if (!user) {
+        return res.status(404).json({ message: "No thought exists with that ID" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
   //CREATE a thought
   //UPDATE a thought by its ID
   //DELETE a thought by its ID
